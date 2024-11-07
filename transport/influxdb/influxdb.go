61a1a744056ada7f6849da39782fc81bf9e32556
Pranav <pranav@Pranavs-MacBook-Pro-2.local>
2024-11-07 16:42:34 -0500

    Fix influx

--- transport/influxdb/influxdb.go
+++ transport/influxdb/influxdb.go
@@ -3,10 +3,13 @@ package influxdb
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/influxdata/influxdb-client-go/v2/api/write"
 	"github.com/netsampler/goflow2/v2/transport"
 	"log"
+	"net/url"
+	"os"
 	"time"
 )
 
@@ -21,12 +24,30 @@ type InfluxDBDriver struct {
 }
 
 func (d *InfluxDBDriver) Prepare() error {
-	// ... (code for setting up URL, token, org, and bucket remains the same)
+	flag.StringVar(&d.url, "transport.influxdb.url", "http://localhost:8086", "InfluxDB URL")
+	log.Print(d.url)
+	flag.StringVar(&d.token, "transport.influxdb.token", os.Getenv("INFLUXDB_TOKEN"), "InfluxDB token")
+	log.Print(d.token)
+	flag.StringVar(&d.org, "transport.influxdb.org", "hi", "InfluxDB organization")
+	log.Print(d.org)
+	flag.StringVar(&d.bucket, "transport.influxdb.bucket", "hi", "InfluxDB bucket")
+	log.Print(d.bucket)
 	return nil
 }
 
 func (d *InfluxDBDriver) Init() error {
-	// ... (code for initializing the InfluxDB client remains the same)
+	_, err := url.Parse(d.url)
+	if err != nil {
+		log.Printf("Invalid InfluxDB URL: %v", err)
+		return err
+	}
+	d.client = influxdb2.NewClient(d.url, d.token)
+	log.Print(d.client)
+	log.Print(d.org)
+	log.Print(d.bucket)
+	log.Print(d.token)
+	log.Print(d.url)
+
 	return nil
 }
 
@@ -36,19 +57,16 @@ func (d *InfluxDBDriver) Errors() <-chan error {
 
 func (d *InfluxDBDriver) Send(key, data []byte) error {
 	var influxData map[string]interface{}
-	err := json.Unmarshal(data, &influxData)
-	if err != nil {
-		return err
+	if err := json.Unmarshal(data, &influxData); err != nil {
+		log.Fatal(err)
 	}
-
 	writeAPI := d.client.WriteAPIBlocking(d.org, d.bucket)
-	point := write.NewPoint("netflow_v5", nil, influxData, time.Now())
+	point := write.NewPoint("netflow", nil, influxData, time.Now())
 	if err := writeAPI.WritePoint(context.Background(), point); err != nil {
 		log.Fatal(err)
 	}
 	return nil
 }
-
 func (d *InfluxDBDriver) Close() error {
 	if d.client != nil {
 		d.client.Close()
